722e8b24f3902f677c122bc4a1508c7edc9b0417
Pushpita <32939907+pushpitab@users.noreply.github.com>
2018-07-11 22:52:44 -0400

    adding comment to trigger ci 3

--- test.js
+++ test.js
@@ -4,5 +4,6 @@ import pbtest1 from '.';
 test('pbtest1()', (t) => {
     //adding test comment
     //adding cooment to trigger ci 
+     //adding cooment to trigger ci 2
     t.notThrows(pbtest1, 'Fix all module errors.');
 });
