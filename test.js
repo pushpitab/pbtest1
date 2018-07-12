655087221b63302935faf9ee087e0c85556b0af5
Pushpita <32939907+pushpitab@users.noreply.github.com>
2018-07-12 00:07:42 -0400

    deleting comments to trigger ci

--- test.js
+++ test.js
@@ -3,7 +3,5 @@ import pbtest1 from '.';
 
 test('pbtest1()', (t) => {
     //adding test comment
-    //adding cooment to trigger ci 
-     //adding cooment to trigger ci 2
     t.notThrows(pbtest1, 'Fix all module errors.');
 });
