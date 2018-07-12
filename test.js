9c03b9f9581fc0db9d8123c659c32bb5a3b5fdfd
Pushpita <32939907+pushpitab@users.noreply.github.com>
2018-07-12 00:26:16 -0400

    comment to trigger ci

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ import test from 'ava';
 import pbtest1 from '.';
 
 test('pbtest1()', (t) => {
-    //adding test comment
+    //adding test comment to trigger ci
     t.notThrows(pbtest1, 'Fix all module errors.');
 });
